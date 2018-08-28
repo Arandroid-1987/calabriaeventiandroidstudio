67b2b68dd32b8b8b3d4dd8e60e0ad02927b48616
arandroidsvn <arandroidsvn@49be7619-83d0-426c-bb54-176d2216121b>
2018-08-28 13:55:45 +0000

    Clear

--- CalabriaEventi/trunk/app/src/main/java/com/calabriaeventi/glide/CalabriaGlideModule.java
+++
@@ -1,8 +0,0 @@
-package com.calabriaeventi.glide;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public class CalabriaGlideModule extends AppGlideModule {
-}
--- CalabriaEventi/trunk/app/src/main/java/com/calabriaeventi/io/AsyncCallback.java
+++
@@ -1,5 +0,0 @@
-package com.calabriaeventi.io;
-
-public interface AsyncCallback<T> {
-    void callback(T t);
-}
